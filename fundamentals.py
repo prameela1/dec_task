f498524d5270e3a52a87f964130a85f68d088601
prameelakothuri <prameela.k5807@gmail.com>
2025-12-19 11:22:33 +0530

    hello

---
+++ fundamentals.py
@@ -0,0 +1,226 @@
+# print --> it is used to died to display information on the screen
+
+#syntax
+#print(exp)
+
+
+#print(10+10)
+#print()
+#print("welcome to pythonlife")
+
+
+# below code is used for addition operation
+#num_1 = 10
+#num_2 = 10
+#result = num_1 + num_2
+#print(result)
+
+###'''
+#above code is used to perform addition operation
+##num_1 is a variable it contains a value 10
+#num_2 is a variable it contains a value 10
+#later, in next line performed addition operation by using + operator
+#result has been displayed by using print statement
+
+#### '''
+
+#syntax for variable creation
+#variable = value
+
+#age = 35
+#print(id(age))
+#product_cost =1000
+#print(id(product_cost)) 
+
+#employee_1_salary = 253
+#print(employee_1_salary)
+
+#name = "arun"
+#Name = "ram"
+#NAME = "arjun"
+
+#print(id(name))
+#print(id(name))
+#print(id(name))
+
+#IF = 'pythonlife"
+#print(IF)
+
+
+
+#1.camel case
+#studentName ="manju"
+#print(studentName)
+
+#2. snake case
+#student_name = "ramya"
+#print(student_name)
+
+
+#3.pascal case
+#StudentName = "muni"
+#print(StudentName)
+
+#product_cost = -35456677
+#print(product_cost)
+#print(type(product_cost))
+
+#
+#height = 5.7
+#print(type(height))
+
+#string --> it is used to represent sequence of characters , it is used to store textual information
+
+#sentence ='welcome to pythonlife'
+#print(sentence)
+#print(type(sentence))
+#sentence_2 = "welcome to pythonlife"
+#print(sentence_2)
+#print(type(sentence_2))
+#sentence_3 = ''' welcome to pythonlife'''
+#print(sentence_3)
+#print(type(sentence_3))
+
+#type conversion ---> implicit(automatic )type convrsion 2. explict type conversion(manual)
+
+#int --> float
+
+#product_cost = 1000
+#print(product_cost)
+#print(type(product_cost)) 
+
+#product_cost = 1000
+#float_conversion = float(product_cost)
+#print(float_conversion)
+#print(type(float_conversion))
+
+#float--> int
+#kilometers = 1.25
+#int_conversion = int(kilometers)
+#print(int_conversion)
+#print(type(int_conversion))
+
+# str ---> int
+#employee_salary = "250000"
+#print(employee_salary)
+#print(type(employee_salary))
+#int_conversion = int(employee_salary)
+#print(type(int_conversion))
+
+
+#str ----> float
+#height = "5.7"
+#print(type(height))
+#float_conversion = float(height)
+#print(float_conversion)
+#print(type(float_conversion))
+
+#num_1 = int(input("enter number 1:"))
+#num_2 =int(input("enter number 2:"))
+#result = num_1+num_2
+#print(result)
+
+#num_1 = 25
+#num_2 =25.30
+#print(num_1 +num_2)
+
+####### nov 10 ####
+
+#LIST[]
+
+
+#sample_list = [1,2.5,"welcome to pythonlife"]
+#print(id(sample_list))
+#print(type(sample_list))
+#sample_list.append("pythonlife")
+#print(sample_list)
+#print(id(sample_list))
+
+# tuples
+#voter_id =(35,2.5,"pythonlife,[1,2,3,4,5]")
+#print(voter_id)
+#print(type(voter_id))
+
+# set mutuable data type
+#sample_set = {35,2.3,"kiran,charan,prasad",45,"charan,"25,(1,2,4)}
+#print(sample_set)
+#print(type(sample_set))
+
+#dict
+#sample_dict{"user":"user@123","user2": "user2@123",}
+#user data ={
+# 1:"vasu",
+# 2:"ram",
+# 3:"raj",
+# 4:"ayesha",
+# 5:"kumar",
+ #1: "raj kumar"
+ #(1,3):"data",
+#"vasu":12345,
+ # [2,3,4]:
+
+
+#bolean True and False
+#sample= True
+#print(sample)
+#print(type(sample))
+
+#sample2 = False
+#print(sample2)
+#print(type(sample2))
+
+#print(bool(1))
+#print(bool(0))
+
+#num_1 = 5+10j
+#print(num_1)
+#print(type(num_1))
+#num_2 = 5+5j
+#print(num_2)
+#print(type(num_2))
+
+#print(num_1+num_2)
+
+#product_data ={
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
